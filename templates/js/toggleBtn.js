0dbe5af63302743abe784f94ca3e0f3f1661d820
cnails <andreyandreevgr@gmail.com>
2020-08-28 14:48:22 +0300

    adaptive now

---
+++ templates/js/toggleBtn.js
@@ -0,0 +1,12 @@
+var navbar = document.querySelector(".navbar");
+var toggle = document.querySelector(".toggle");
+document.addEventListener("click", function(e) {
+    if (navbar.classList.contains("navbar-active")) {
+        if (e.target !== navbar && !navbar.contains(e.target) && !toggle.contains(e.target) && toggle !== e.target) {
+            navbar.classList.remove("navbar-active");
+        }
+    }
+});
+document.querySelector(".toggle").addEventListener("click", function(e) {
+    navbar.classList.toggle("navbar-active");
+});
