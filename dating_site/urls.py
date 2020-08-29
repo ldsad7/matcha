314a93ded526423e66022d7f978cb8ec53264566
tsimonis <happypuffin7@gmail.com>
2020-08-29 22:02:33 +0300

    + improved html templates

--- dating_site/urls.py
+++ dating_site/urls.py
@@ -5,6 +5,9 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
+from matcha import views
+
+
 schema_view = get_schema_view(
     openapi.Info(
         title="API",
@@ -15,10 +18,14 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('search/', views.search, name='search'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('matcha.urls')),
     path(
         'api/v1/docs/', schema_view.with_ui(cache_timeout=0), name='API'
     ),
     path('accounts/', include('registration.backends.default.urls')),
+    path('accounts/profile/', views.profile, name='profile'),
 ]
