2073c5b5e3ad114911f1dddb5e15cbcf2dc8e745
tsimonis <happypuffin7@gmail.com>
2020-08-29 16:24:55 +0300

    + added image view

--- dating_site/urls.py
+++ dating_site/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -21,4 +23,4 @@ urlpatterns = [
         'api/v1/docs/', schema_view.with_ui(cache_timeout=0), name='API'
     ),
     path('accounts/', include('registration.backends.default.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
