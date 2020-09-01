ba5875b80bf3a4a7e41d476638f0b436a3a12c62
cnails <andreyandreevgr@gmail.com>
2020-09-01 10:08:18 +0300

    fixed some bugs

--- matcha/static/js/appendImage.js
+++ matcha/static/js/appendImage.js
@@ -13,25 +13,26 @@ var image = {
         output.style.display = "inline-block";
         output.src = URL.createObjectURL(event.target.files[0]);
         output.removeAttribute("id");
-        output.onload = function() {
-            URL.revokeObjectURL(output.src) // free memory
-            avatar.edit();
-            // var ajax = new XMLHttpRequest();
-            $.ajax({
-                url: '/api/v1/user_photos', // url where to submit the request
-                type : "POST", // type of action POST || GET
-                dataType : 'json', // data type
-                data : $("#form").serialize(), // post data || get data
-                success : function(result) {
-                    // you can see the result from the console
-                    // tab of the developer tools
-                    console.log(result);
-                },
-                error: function(xhr, resp, text) {
-                    console.log(xhr, resp, text);
-                }
-            })
-        }
+        avatar.edit();
+        // output.onload = function() {
+        //     URL.revokeObjectURL(output.src) // free memory
+        //     avatar.edit();
+        //     // var ajax = new XMLHttpRequest();
+        //     $.ajax({
+        //         url: '/api/v1/user_photos', // url where to submit the request
+        //         type : "POST", // type of action POST || GET
+        //         dataType : 'json', // data type
+        //         data : $("#form").serialize(), // post data || get data
+        //         success : function(result) {
+        //             // you can see the result from the console
+        //             // tab of the developer tools
+        //             console.log(result);
+        //         },
+        //         error: function(xhr, resp, text) {
+        //             console.log(xhr, resp, text);
+        //         }
+        //     })
+        // }
     },
     edit: function() {
         this.input = document.getElementById("image-input");
