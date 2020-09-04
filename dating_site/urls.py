795cbb842c42547c4516e01ecb88dc67a083d5b9
tsimonis <happypuffin7@gmail.com>
2020-09-04 16:45:23 +0300

    + added debug toolbar

--- dating_site/urls.py
+++ dating_site/urls.py
@@ -32,3 +32,9 @@ urlpatterns = [
     path('accounts/', include('registration.backends.default.urls')),
     path('accounts/profile/', views.profile, name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
