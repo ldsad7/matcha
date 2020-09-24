dae14ea7baad3234651dc08b35395b4e4f877091
tsimonis <happypuffin7@gmail.com>
2020-09-24 15:37:40 +0300

    * user -> global_user, added a context_processor

--- chat/views.py
+++ chat/views.py
@@ -7,6 +7,6 @@ def index(request):
 
 def room(request, room_name):
     context = {
-        'room_name': room_name
+        'room_name': room_name,
     }
     return render(request, 'chat/room.html', context)
