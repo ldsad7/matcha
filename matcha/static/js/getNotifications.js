ae30103f8c7abea6914bcfdc5cd5e9b72f351c9e
cnails <cnails@student.21-school.ru>
2020-09-25 17:00:27 +0300

    fixed date

--- matcha/static/js/getNotifications.js
+++ matcha/static/js/getNotifications.js
@@ -1,4 +1,6 @@
-notif_data = [];
+function getLocalNotifs() {
+    window.localStorage.getItem("notif")
+}
 
 function clearNotifList() {
     document.querySelectorAll("#notif_list a").forEach(el => {
@@ -6,20 +8,26 @@ function clearNotifList() {
     });
 }
 
-const appendNotif = (text) => {
+const appendNotif = (text, href) => {
     const newNotif = document.createElement("a");
-    newNotif.setAttribute("href", "#");
+    newNotif.setAttribute("href", href || "#");
     newNotif.innerHTML = `<span class='notif_elem'>${text}</span>`
-    newNotif.addEventListener("mouseenter", function(e) {setTimeout(() => this.remove(), 5000)});
+    newNotif.addEventListener("mouseenter", function(e) {_idTimeout = setTimeout(() => this.remove(), 1500)});
+    newNotif.addEventListener("mouseleave", function(e) {clearInterval(_idTimeout)});
     document.getElementById("notif_list").appendChild(newNotif);
 }
 
 function changeNotifList() {
     SendRequest("get", "/api/v1/notifications", "created=" + (Math.floor(+new Date() / 1000) - 10), function(e) {
         const result = JSON.parse(e.response);
+        console.log(result);
         result.forEach(el => {
             const { type } = el;
-            appendNotif(type);
+            if (el.user_1) {
+                appendNotif(type, "/profiles/" + el.user_1.id);
+            } else {
+                appendNotif(type);
+            }
         });
     });
 }
