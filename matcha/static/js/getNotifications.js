344f9af28cf22d9b7161349953ceee3644334622
cnails <cnails@student.21-school.ru>
2020-09-25 20:20:08 +0300

    notifications fixed

--- matcha/static/js/getNotifications.js
+++ matcha/static/js/getNotifications.js
@@ -1,3 +1,5 @@
+_intervalNotification = 5; // seconds
+
 function getLocalNotifs() {
     window.localStorage.getItem("notif")
 }
@@ -11,14 +13,23 @@ function clearNotifList() {
 const appendNotif = (text, href) => {
     const newNotif = document.createElement("a");
     newNotif.setAttribute("href", href || "#");
+    newNotif.classList.add("animate__animated");
     newNotif.innerHTML = `<span class='notif_elem'>${text}</span>`
-    newNotif.addEventListener("mouseenter", function(e) {_idTimeout = setTimeout(() => this.remove(), 1500)});
+    newNotif.addEventListener("mouseenter", function(e) {
+        setTimeout(() => {
+            _idTimeout = setTimeout(
+                () => {
+                    this.classList.add("animate__fadeOutTopRight");
+                    setTimeout(() => {this.remove()}, 700)
+                }, 1500)
+        }, 0);
+    });
     newNotif.addEventListener("mouseleave", function(e) {clearInterval(_idTimeout)});
     document.getElementById("notif_list").appendChild(newNotif);
 }
 
 function changeNotifList() {
-    SendRequest("get", "/api/v1/notifications", "created=" + (Math.floor(+new Date() / 1000) - 10), function(e) {
+    SendRequest("get", "/api/v1/notifications", "created=" + (Math.floor(+new Date() / 1000) - _intervalNotification), function(e) {
         const result = JSON.parse(e.response);
         console.log(result);
         result.forEach(el => {
@@ -35,7 +46,7 @@ function changeNotifList() {
 function getNotifications(e) {
     setInterval(() => {
         changeNotifList();
-    }, 10000);
+    }, _intervalNotification * 1000);
 }
 
 getNotifications();
